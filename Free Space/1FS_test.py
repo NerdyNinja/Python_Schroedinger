0c6b85477e3dd3bbaa02fa3c3f43ca373905f746
NerdyNinja <73026285+NerdyNinja@users.noreply.github.com>
2021-09-19 21:47:38 +0200

    1KÜ_Print added with new folder structure

--- Free Space/1FS_test.py
+++ Free Space/1FS_test.py
@@ -1,5 +1,7 @@
-Number:int = input("Rate eine Zahl ")
+Number = input("Rate eine Zahl ")
 if Number <= "5":
     print("Fuck off")
 elif Number >= "6":
     print("Guter Junge!")
+#elif Number != str:
+   # print("Das keine Zahl du Lelek")
---
+++ Kapitel1/Übungen/1KÜ_Print.py
@@ -0,0 +1,29 @@
+x = "Hallo, "
+y = "Welt!"
+print(x + y)
+
+x = "Hallo, "
+y = "Welt!"
+z = x + y
+print(z)
+
+x, y = "Hallo, ", "Welt!"
+z = x + y
+print(z)
+
+a, b = "Lelek", "du Kek"
+print(a,b)
+
+
+c, d = "Fuck", "da Police"
+print(c,d, sep='*')
+
+h, j = "Hot", "Britney"
+print(h)
+print(j)
+
+f, t = "Geile", "Milfs"
+print(f, end=" ")
+print(t)
+
+
