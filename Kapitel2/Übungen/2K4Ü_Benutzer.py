72e46eebd3d78db32a1da9a4d73b41871fe1fff6
NerdyNinja <73026285+NerdyNinja@users.noreply.github.com>
2022-01-01 13:35:50 +0100

    added UserLoginV1

---
+++ Kapitel2/Übungen/2K4Ü_Benutzer.py
@@ -0,0 +1,8 @@
+name = str(input("Bitte geb einen Namen ein: "))
+
+if name == "Schrödinger":
+    print("Moin", name)
+elif name == "":
+    print("Leer")
+else:
+    print("Du bist der falsche, ", name)
